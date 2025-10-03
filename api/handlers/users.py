f9c60a176e7979bb843b9c461cf7eec99c1f8744
Антон Елесин <elr1c.edw180@gmail.com>
2025-10-03 13:27:37 +0400

    add_user

--- api/handlers/users.py
+++ api/handlers/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from service.database import get_users as get_users_service
-
+from service.database import add_user as add_user_service
 
 router = APIRouter(prefix="/users", tags=['users'])
 
@@ -11,5 +11,5 @@ async def get_users():
 
 @router.post("/add_user")
 async def add_user(chat_id: int, username: str):
-    user = await add_user(chat_id, username)
+    user = await add_user_service(chat_id, username)
     return user
