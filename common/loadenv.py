fb78e154a558704629050f2eca063fe639fb1568
Антон Елесин <elr1c.edw180@gmail.com>
2025-10-09 17:41:38 +0400

    alembic era

--- common/loadenv.py
+++ common/loadenv.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv, dotenv_values
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-print(dotenv_path)
 def returnToken():
     if os.path.exists(dotenv_path):
         return dotenv_values(dotenv_path)['BOT_TOKEN']
